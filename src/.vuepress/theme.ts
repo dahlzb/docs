dd516efcd549dd866002075fe6f07330b9923578
dahlzb <1304187196@qq.com>
2024-04-22 19:57:08 +0800

    配置完善

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://mister-hope.github.io",
+  hostname: "https://dahlzb.github.io/docs",
 
   author: {
 		name: "DahlZb",
